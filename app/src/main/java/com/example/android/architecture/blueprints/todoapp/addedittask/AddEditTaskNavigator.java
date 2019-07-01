9d148496c99ad3a6aedfa5e9f10d425ed493aae7
Ashutosh <ashutoshmca13@gmail.com>
2019-07-01 12:14:22 +0530

    initial commit (Actually second)

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskNavigator.java
@@ -0,0 +1,27 @@
+package com.example.android.architecture.blueprints.todoapp.addedittask;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+
+import com.example.android.architecture.blueprints.todoapp.util.providers.BaseNavigator;
+
+/**
+ * Defines the navigation actions that can be called from AddEditTask screen
+ */
+public class AddEditTaskNavigator {
+
+    @NonNull
+    private final BaseNavigator mNavigationProvider;
+
+    public AddEditTaskNavigator(@NonNull BaseNavigator navigationProvider) {
+        mNavigationProvider = navigationProvider;
+    }
+
+    /**
+     * When the task was saved, the activity should finish with success.
+     */
+    public void onTaskSaved() {
+        mNavigationProvider.finishActivityWithResult(Activity.RESULT_OK);
+    }
+}
---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/schedulers/BaseSchedulerProvider.java
@@ -0,0 +1,21 @@
+package com.example.android.architecture.blueprints.todoapp.util.schedulers;
+
+
+import androidx.annotation.NonNull;
+
+import rx.Scheduler;
+
+/**
+ * Allow providing different types of {@link Scheduler}s.
+ */
+public interface BaseSchedulerProvider {
+
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler ui();
+}
